e8d4849f40c82d17268a73aba22a40ca02f3504a
jaseg <code@jaseg.net>
2015-11-02 20:57:41 +0100

    Added --archive option
    Fixes #1

--- fetch-gutmann.py
+++ fetch-gutmann.py
@@ -3,44 +3,55 @@
 import tempfile, requests, subprocess, re, argparse
 from datetime import datetime
 from decimal import Decimal
+from contextlib import contextmanager
+import os.path
 
-def fetch(location='MA'):
-	with open('Kantine {} {}.pdf'.format(location, datetime.now().strftime('%Y-%m-%d %H-%M-%S')), 'w+b') as orig:
-		res = requests.get('http://personalkantine.personalabteilung.tu-berlin.de/pdf/{}-aktuell.pdf'.format(location))
-		orig.write(res.content)
-		with tempfile.NamedTemporaryFile() as out:
-			subprocess.check_output(['pdfcrop', '--margins', '-80 -160 -60 -140', orig.name, out.name])
-			c = subprocess.check_output(['venv2/bin/pdf2txt.py', '-p', '1', out.name]).decode('UTF-8')
+def fetch(location='MA', archive=None):
+    @contextmanager
+    def afile():
+        if archive:
+            fn = 'Kantine {} {}.pdf'.format(location, datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
+            yield open(os.path.join(archive, fn), 'w+b')
+        else:
+            yield tempfile.NamedTemporaryFile()
 
-	l = re.split('Montag|Dienstag|Mittwoch|Donnerstag|Freitag', c)[3:]
+    with afile() as orig:
+        res = requests.get('http://personalkantine.personalabteilung.tu-berlin.de/pdf/{}-aktuell.pdf'.format(location))
+        orig.write(res.content)
+        with tempfile.NamedTemporaryFile() as out:
+            subprocess.check_output(['pdfcrop', '--margins', '-80 -160 -60 -140', orig.name, out.name])
+            c = subprocess.check_output(['venv2/bin/pdf2txt.py', '-p', '1', out.name]).decode('UTF-8')
 
-	res = (('^\W*[,.]?\W*([0-9. ]{4,9})?\W*', ''),
-	       ('“\W*([^“]*\w)\W*“', '“\\1“'),
-	       ('\\(\W*', '('),
-	       ('\W*\\)', ')'),
-	       (' ([a-zäöüß]) ', '\\1'),
-	       ('\s+', ' '),
-	       ('\s*([-/])\s*', '\\1'))
+    l = re.split('Montag|Dienstag|Mittwoch|Donnerstag|Freitag', c)[3:]
 
-	def subs(s):
-		for pat,repl in res:
-			s = re.sub(pat, repl, s)
-		return s
+    res = (('^\W*[,.]?\W*([0-9. ]{4,9})?\W*', ''),
+           ('“\W*([^“]*\w)\W*“', '“\\1“'),
+           ('\\(\W*', '('),
+           ('\W*\\)', ')'),
+           (' ([a-zäöüß]) ', '\\1'),
+           ('\s+', ' '),
+           ('\s*([-/])\s*', '\\1'))
 
-	e = [[ subs(v).strip() for v in vs if not 'Jodsalz' in v and v.strip()] for vs in [e.split('€') for e in l]]
-	f = [[(' '.join(title), Decimal(price.replace(',', '.'))) for *title, price in [el.split() for el in k]] for k in e]
-	return f
+    def subs(s):
+        for pat,repl in res:
+            s = re.sub(pat, repl, s)
+        return s
+
+    e = [[ subs(v).strip() for v in vs if not 'Jodsalz' in v and v.strip()] for vs in [e.split('€') for e in l]]
+    f = [[(' '.join(title), Decimal(price.replace(',', '.'))) for *title, price in [el.split() for el in k]] for k in e]
+    return f
 
 def print_week(week):
-	for i,day in enumerate(week):
-		print(('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag')[i])
-		print('--------');
-		for meal, price in day:
-			print('{:<80} {:02f}'.format(meal, price))
-		print()
+    for i,day in enumerate(week):
+        print(('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag')[i])
+        print('--------');
+        for meal, price in day:
+            print('{:<80} {:02f}'.format(meal, price))
+        print()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('location', default='MA', nargs='?')
+parser.add_argument('-a', '--archive', help='Keep archival copy of source document under given path')
 args = parser.parse_args()
 
-print_week(fetch(args.location))
+print_week(fetch(args.location, args.archive))
--- fetch-studentenwerk.py
+++ fetch-studentenwerk.py
@@ -3,45 +3,51 @@
 import bs4, requests, argparse
 from decimal import Decimal
 from datetime import datetime
+import os.path
 
 
-def fetch(location='tu', veg=True):
-	res = requests.get('http://www.studentenwerk-berlin.de/speiseplan/rss/{}/woche/lang/{}'.format(location, int(veg)))
-	with open('Mensa {} {}.rss'.format(location, datetime.now().strftime('%Y-%m-%d %H-%M-%S')), 'w+') as orig:
-		orig.write(res.text)
-	soup = bs4.BeautifulSoup(res.text, 'lxml')
-	soup = bs4.BeautifulSoup(soup.find('item').find('description').text, 'lxml')
+def fetch(location='tu', veg=True, archive=None):
+    res = requests.get('http://www.studentenwerk-berlin.de/speiseplan/rss/{}/woche/lang/{}'.format(location, int(veg)))
+    if archive:
+        fn = 'Mensa {} {}.rss'.format(location, datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
+        with open(os.path.join(archive, fn), 'w+') as orig:
+            orig.write(res.text)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    soup = bs4.BeautifulSoup(soup.find('item').find('description').text, 'lxml')
 
-	format_shit = lambda el: [(f.find('strong').text, Decimal(f.find(class_='mensa_preise').text.split()[1])) for f in el.findAll(class_='mensa_speise')]
+    format_shit = lambda el: [
+            (f.find('strong').text, Decimal(f.find(class_='mensa_preise').text.split()[1]))
+            for f in el.findAll(class_='mensa_speise')]
 
-	ft = lambda t: [format_shit(e) for e in soup.findAll(class_=t)]
+    ft = lambda t: [format_shit(e) for e in soup.findAll(class_=t)]
 
-	special, food, soups = map(ft, ('special', 'food', 'soups'))
+    special, food, soups = map(ft, ('special', 'food', 'soups'))
 
-	week = list(zip(special, food, soups))
+    week = list(zip(special, food, soups))
 
-	return week
+    return week
 
 def print_week(week):
-	for i,(a,b,c) in enumerate(week):
-		print(('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag')[i])
-		print('--------');
-		print('~ Special shit ~')
-		for meal, price in a:
-			print('{:<80} {:02f}'.format(meal, price))
-		print()
-		print('~ Ordinary shit ~')
-		for meal, price in b:
-			print('{:<80} {:02f}'.format(meal, price))
-		print()
-		print('~ Soup shit ~')
-		for meal, price in c:
-			print('{:<80} {:02f}'.format(meal, price))
-		print()
+    for i,(a,b,c) in enumerate(week):
+        print(('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag')[i])
+        print('--------');
+        print('~ Special shit ~')
+        for meal, price in a:
+            print('{:<80} {:02f}'.format(meal, price))
+        print()
+        print('~ Ordinary shit ~')
+        for meal, price in b:
+            print('{:<80} {:02f}'.format(meal, price))
+        print()
+        print('~ Soup shit ~')
+        for meal, price in c:
+            print('{:<80} {:02f}'.format(meal, price))
+        print()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('location', default='tu', nargs='?')
 parser.add_argument('-V', '--vegetarian', action='store_true')
+parser.add_argument('-a', '--archive', help='Keep archival copy of source document under given path')
 args = parser.parse_args()
 
-print_week(fetch(args.location, args.vegetarian))
+print_week(fetch(args.location, args.vegetarian, args.archive))
